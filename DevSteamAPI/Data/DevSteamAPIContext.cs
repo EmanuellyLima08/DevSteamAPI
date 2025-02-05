aa0d531971fa68b10472f1dfd50763e5be9d8fd3
Emanuelly <emanuellydssenai@gmail.com>
2025-02-05 14:41:14 -0300

    Inclusão dos EndPoints do Identity Framework

--- DevSteamAPI/Data/DevSteamAPIContext.cs
+++ DevSteamAPI/Data/DevSteamAPIContext.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevSteamAPI.Data
 {
-    public class DevSteamAPIContext : DbContext
+    public class DevSteamAPIContext : IdentityDbContext
     {
+        //Método Construtor
+        public DevSteamAPIContext(DbContextOptions<DevSteamAPIContext> options) : base(options)
+        {
+        }
+
+        //Sobrescever o método OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
